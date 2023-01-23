032fb901a6e3a274447e8da98a3bfb489feeebc6
Stanislav Grokhotov <grokhotov@isands.ru>
2023-01-23 13:33:58 +0300

    first commit

---
+++ src/test/java/com/youngstanis/filestorage/FileStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youngstanis.filestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
